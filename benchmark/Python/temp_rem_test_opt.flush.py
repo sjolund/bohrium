418e97102225639be7a798a6de5dc5fee2e0cd66
Johannes Lund <johannes.lund@gmail.com>
2012-12-05 11:48:42 +0100

    temp array testing with flush inserts to better compare JIT and naive

---
+++ benchmark/Python/temp_rem_test_opt.flush.py
@@ -0,0 +1,25 @@
+import cphvbnumpy as np
+import cphvbbridge
+import util
+
+B = util.Benchmark()
+H = B.size[0]
+W = B.size[1]
+I = B.size[2]
+
+a = np.ones((H,W),       dtype=B.dtype, cphvb=B.cphvb)
+b = np.ones((H,W),       dtype=B.dtype, cphvb=B.cphvb)
+c = np.ones((H,W),       dtype=B.dtype, cphvb=B.cphvb)
+d = np.ones((H,W),       dtype=B.dtype, cphvb=B.cphvb)
+e = np.ones((H,W),       dtype=B.dtype, cphvb=B.cphvb)
+
+B.start()
+for i in xrange(I):
+    cphvbbridge.flush()
+    a += b
+    a += c
+    a += d
+    a += e
+    
+B.stop()
+B.pprint()
