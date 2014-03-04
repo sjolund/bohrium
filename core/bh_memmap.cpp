6916a7bf9758d1355a5e2cb454fe6459f67c55ee
Morten Rasmussen <cruise@diku.dk>
2014-03-04 09:51:23 +0100

    Adds skeleton code for the bh_memmap library
    This commit contains the header and cpp file for the bh_library.
    At the current state the implementation is scarcely finished.
    
    It supports the correct mprotect calls for the new file mapped arrays.

---
+++ core/bh_memmap.cpp
@@ -0,0 +1,133 @@
+/*
+This file is part of Bohrium and copyright (c) 2012 the Bohrium
+team <http://www.bh107.org>.
+
+Bohrium is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3
+of the License, or (at your option) any later version.
+
+Bohrium is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the
+GNU Lesser General Public License along with Bohrium.
+
+If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <bh_memmap.h>
+
+
+
+/** Initialize the memmap module.
+ *
+ * @return Error code (BH_SUCCESS, BH_OUT_OF_MEMORY)
+ */
+bh_error bh_init_memmap(void)
+{
+    printf("bh_memmap : Init of library\n");
+
+    return BH_SUCCESS;
+}
+
+/** Create a virtual memory area mapped to a file
+ *
+ * @param fp bh_array containing the file path.
+ * @param bhtype Data type of the array.
+ * @param mode File access mode, string, "r+", "r", "w+"
+ * @param offset int where the 
+ * @param shape Shape of the array which it should be translated into
+ * @param order Array ordering, row or column
+ * @return Error code (BH_SUCCESS, BH_OUT_OF_MEMORY)
+ */
+bh_error bh_create_memmap(bh_instruction *instr)
+{
+    bh_view *operands = bh_inst_operands(instr);
+    bh_int64* fargs = (bh_int64*)(operands[1].base->data);
+
+    // Parse file arguments
+    char* fpath = (char*)(operands[0].base->data);
+    bh_int64 mode = fargs[0];
+    bh_intp offset = (bh_intp)fargs[1];
+    bh_int64 order = fargs[2];
+
+    int fileflag = O_RDWR;
+    // From mode create the right set of file flags
+    if (mode == 0 || mode == 3)
+    {
+        fileflag = O_RDONLY;
+    }
+    else if (mode == 2) {
+        fileflag |= O_TRUNC;
+    }
+    bh_intp size_in_bytes = bh_base_size(operands[2].base);
+    // Open file with the right parameters
+    int fd = open(fpath, fileflag | O_CREAT, (mode_t)0600);
+    struct stat sb;
+
+    // Stretch the file size to the correct size according to the bh_base array
+    // Raise errors if things is rotten.
+    if (lseek(fd, size_in_bytes-1, SEEK_SET) == -1)
+    {
+        fprintf(stderr, "bh_create_memmap() could not resize file:\"%s\"\n", fpath);
+        return BH_ERROR;
+    }
+    if (write(fd, "", 1) == -1)
+    {
+        fprintf(stderr, "bh_create_memmap() could not write to file \"%s\"\n", fpath);
+        return BH_ERROR;
+    }
+    printf("BEFORE: %p \n", operands[2].base->data);
+    // mmap virtual address space for array data
+    operands[2].base->data = bh_memory_malloc(size_in_bytes);
+    printf("AFTER: %p \n", operands[2].base->data);
+    // Attach fd and together with address space to the signal handler
+    // mprotect base->data, to make sure that future access to the array will be handled by custom signal handler
+    mprotect((void *)operands[2].base->data, size_in_bytes, PROT_NONE);
+    return BH_SUCCESS;
+}
+
+/** Destroy a virtual mapping of a file to 
+ *
+ * @param ary bh_array file mapped array.
+ * @return Error code (BH_SUCCESS, BH_OUT_OF_MEMORY)
+ */
+bh_error bh_destroy_memmap(bh_base ary)
+{
+    return BH_SUCCESS;
+}
+
+/** Sync the content of the filemapped array to disk
+ *
+ * @param ary bh_array file mapped array.
+ * @return Error code (BH_SUCCESS, BH_OUT_OF_MEMORY)
+ */
+bh_error bh_sync_memmap(bh_base ary)
+{
+    return BH_SUCCESS;
+}
+
+/** Adds a hint to the I/O queue in form of a execution list.
+ *
+ * @return Error code (BH_SUCCESS, BH_OUT_OF_MEMORY)
+ */
+bh_error bh_hint_memmap()
+{
+    return BH_SUCCESS;
+}
+
+/** Callback attached to the bohrium signal handler.
+ *
+ *  It will handle the case when the array segment is only on disk.
+ *
+ * @return void
+ */
+void bh_sighandler_memmap(unsigned long idx, uintptr_t addr)
+{
+    // First iteration; Idx will be the file handler.
+
+
+}
---
+++ include/bh_memmap.h
@@ -0,0 +1,106 @@
+/*
+This file is part of Bohrium and copyright (c) 2012 the Bohrium
+team <http://www.bh107.org>.
+
+Bohrium is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 3 
+of the License, or (at your option) any later version.
+
+Bohrium is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the 
+GNU Lesser General Public License along with Bohrium. 
+
+If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __BH_MEMMAP_H
+#define __BH_MEMMAP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <fcntl.h>
+#include "bh.h"
+#include "bh_type.h"
+#include "bh_array.h"
+#include "bh_instruction.h"
+#include "bh_error.h"
+
+
+#define PAGE_SIZE getpagesize()
+#define PAGE_ALIGN(address) ((uintptr_t) (((uintptr_t)address) & (~((uintptr    _t)(PAGE_SIZE-1)))))
+
+long int BH_MEMMAP_OPCODE = -1;
+
+
+/** Initialize the memmap module.
+ *
+ * @return Error code (BH_SUCCESS, BH_OUT_OF_MEMORY)
+ */
+bh_error bh_init_memmap(void);
+
+
+/** Create a virtual memory area mapped to a file.
+ *
+ * @param fp bh_array containing the file path
+ * @param bhtype Data type of the array
+ * @param mode File access mode, string, "r+", "r", "w+"
+ * @param offset int offset from where the file should be handled
+ * @param shape Shape of the array which it should be translated into
+ * @param order Array ordering, row or column
+ * @param new_base The handler for the new base array mapped to a file
+ * @return Error code (BH_SUCCESS, BH_OUT_OF_MEMORY)
+ */
+bh_error bh_create_memmap(bh_instruction *instr);
+
+
+/** Destroy a virtual mapping of a file to array.
+ *
+ * @param ary bh_array file mapped array.
+ * @return Error code (BH_SUCCESS, BH_OUT_OF_MEMORY)
+ */
+bh_error bh_destroy_memmap();
+
+
+/** Sync the content of the filemapped array to disk.
+ *
+ * @param ary bh_array file mapped array.
+ * @return Error code (BH_SUCCESS, BH_OUT_OF_MEMORY)
+ */
+bh_error bh_sync_memmap();
+
+
+/** Adds a hint to the I/O queue in form of a execution list.
+ *
+ * @return Error code (BH_SUCCESS, BH_OUT_OF_MEMORY)
+ */
+bh_error bh_hint_memmap();
+
+
+/** Callback attached to the bohrium signal handler.
+ *
+ *  It will handle the case when the array segment is only on disk.
+ *
+ * @return void
+ */
+void bh_sighandler_memmap(unsigned long idx, uintptr_t addr);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
