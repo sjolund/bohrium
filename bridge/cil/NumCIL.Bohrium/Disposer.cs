5411f5c1924d8b1417e079594f36d5d9d56f768e
Kenneth Skovhede <kenneth@hexad.dk>
2015-02-18 14:56:02 +0100

    Testing branch where the GC will choose when to discard items. Should be used with the environment variable BH_GC_FLUSH=1 to ensure discards are sent before running a flush, otherwise discards will span multiple flushes.

---
+++ bridge/cil/NumCIL.Bohrium/Disposer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NumCIL.Bohrium
+{
+	public static class Disposer
+	{
+		public static T Create<T>(T item) 
+			where T : IDisposable
+		{
+			return new DisposerImpl<T>(item).Child;
+		}
+	}
+
+	public class DisposerImpl<T>
+		where T : IDisposable
+	{
+
+		public T Child { get; private set; }
+		private bool m_hasDisposed = false;
+
+		public DisposerImpl(T child)
+		{
+			Child = child;
+		}
+
+		~DisposerImpl()
+		{
+			if (!m_hasDisposed)
+			{
+				m_hasDisposed = true;
+				Child.Dispose();
+			}
+		}
+	}
+}
+
