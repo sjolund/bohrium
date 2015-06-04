278575070cc31c7672d6fc5f6c4dcb407a812237
Christian Sjølund <chrsjolund@gmail.com>
2015-06-04 20:09:57 +0200

    testing power-multiply precision

--- benchmark/Python/preprocessor_benchmark_settings.py
+++ benchmark/Python/preprocessor_benchmark_settings.py
@@ -2,12 +2,12 @@
 def to_run():
 
     to_run = []
-    to_run.append("power")
+    #to_run.append("power")
     #to_run.append("powerodd")
-    #to_run.append("powercorrectness")
+    to_run.append("powercorrectness")
     #to_run.append("black_scholes")
     
-    to_run.append("common")
+    #to_run.append("common")
     #to_run.append("common4")
     #to_run.append("common3d")
     #to_run.append("commonexp")
